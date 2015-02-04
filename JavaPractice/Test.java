e829a2fa4fc1e03837c5360524df7264109cf5c7
COoder_Y <yangruihan@vip.qq.com>
2015-02-04 21:18:10 +0800

    15_2_4

--- JavaPractice/Test.java
+++ JavaPractice/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		Date date = new Date(2015, 2, 3);
 		System.out.printf("%te", date);
+
+		System.out.println("this is a good example");
 	}
 }
 
